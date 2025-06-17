5afdfa9f013beb3ac380cd63896638e7eb265ce5
BAhaa Enab <bahaenab@gmail.com>
2025-06-17 14:55:36 +0300

    index

--- TurboClean/src/main/java/com/TurboClean/models/User.java
+++ TurboClean/src/main/java/com/TurboClean/models/User.java
@@ -1,5 +1,137 @@
 package com.TurboClean.models;
 
-public class User {
+import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@Size(min = 3, max = 30, message = "First Name must be more than 3 characters")
+	private String firstName;
+
+	@NotNull
+	@Size(min = 3, max = 30, message = "Last Name must be more 3 characters")
+	private String lastName;
+	
+	@NotNull
+	@Size(min = 8, max = 200, message = "The password must be between 8 and 200 characters")
+	private String password;
+
+	@Transient
+	@NotEmpty(message = "Confirm Password is required!")
+	@Size(min = 8, max = 128, message = "Confirm Password must be between 8 and 128 characters")
+	private String confirmPassword;
+
+	@NotNull(message = "Email cannot be null")
+	@Email(message = "Invalid email address format")
+	@Column(unique = true , nullable = false)
+	private String email;
+
+
+	// This will not allow the createdAt column to be updated after creation
+	@Column(updatable = false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date createdAt;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date updatedAt;
+	
+
+	public User() {
+
+	}
+
+	public User(String firstName, String lastName, String password, String confirmPassword, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.confirmPassword = confirmPassword;
+		this.password = password;
+		this.email = email;
+	}
+
+// other getters and setters removed for brevity
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
+
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getlastName() {
+		return lastName;
+	}
+
+	public void setlastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	
 }
