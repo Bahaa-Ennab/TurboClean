930cb98701455bc72c6098b2a48a416e48d05769
ABDALLAH S. FREHAT <frehat.abdallah99@gmail.com>
2025-06-17 10:01:48 +0300

    starting project

---
+++ TurboClean/src/main/java/com/TurboClean/TurboCleanApplication.java
@@ -0,0 +1,13 @@
+package com.TurboClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurboCleanApplication.class, args);
+	}
+
+}
